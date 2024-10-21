f67d38af5e9e50da4e6899178cc8f071b2014c38
Roj Tamang <tamangroj56@gmail.com>
2024-10-21 22:17:24 +0545

    Changes in project view

--- project/views.py
+++ project/views.py
@@ -11,4 +11,16 @@ class ProjectView(APIView):
         project = Project.objects.all()
         serializer = ProjectSerializer(project, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            try: 
+                project = Project.objects.get(id)
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response({"message": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
 
